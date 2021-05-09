35cce70c1029a8468ee8d0f899c3c2558a11ae78
albenchris <albenchris00@gmail.com>
2021-05-09 14:15:50 -0500

    wrote passing test for UPDATE_CATEGORIES case of reducer function

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -10,7 +10,14 @@ export const reducer = (state, action) => {
         case UPDATE_PRODUCTS:
             return {
                 ...state,
-                products: [...action.products],
+                products: [...action.products]
+            };
+        
+        // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+        case UPDATE_CATEGORIES:
+            return {
+                ...state,
+                categories: [...action.categories]
             };
 
         // if it's none of these actions, do not update the state at all and keep things the same!
