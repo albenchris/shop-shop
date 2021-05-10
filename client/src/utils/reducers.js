3effa28d892d07b0942bb6ae1e9fd523def88092
albenchris <albenchris00@gmail.com>
2021-05-10 09:11:22 -0500

    wrote passing test for UPDATE_CART_QUANTITY

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -60,6 +60,18 @@ export const reducer = (state, action) => {
                 cartOpen: newState.length > 0,
                 cart: newState                
             };
+
+        case UPDATE_CART_QUANTITY:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: state.cart.map(product => {
+                    if (action._id === product._id) {
+                        product.purchaseQuantity = action.purchaseQuantity;
+                    }
+                    return product;
+                })
+            };
         // ========= CART CASES END =============================================================================================================
 
         // if it's none of these actions, do not update the state at all and keep things the same!
